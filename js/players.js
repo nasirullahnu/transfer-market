041f42bdf3730bcb68fd60a8a790b33d58f6852d
nasirullahnu <nasirullah878@gmail.com>
2022-08-23 04:00:59 +0600

    player select button common function added

--- js/players.js
+++ js/players.js
@@ -1,2 +1,31 @@
+// document.getElementById('select-buttons').addEventListener('click', function (event) {
+//     const selectButons = event.target.innerText;
+//     const targetPlayer = document.getElementsByTagName('h3');
+//     const targetPlayerName = targetPlayer.innerText;
+//     console.log(targetPlayerName);
+//     console.log(selectButons);
 
-console.log('this is connected');
+// })
+
+
+function getPlayerName(playerName){
+    const yourComment = document.getElementById(playerName);
+    const inputText = yourComment.innerText;
+
+    const commentContainer = document.getElementById('sellected-players');
+    const para = document.createElement('p');
+    para.innerText = inputText;
+    commentContainer.appendChild(para);
+
+}
+
+document.getElementById('btn-messi').addEventListener('click', function () {
+    getPlayerName('player-1');
+})
+
+
+document.getElementById('btn-ronaldo').addEventListener('click',function(){
+    getPlayerName('player-2');
+    
+})
+
