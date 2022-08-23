0bf27bbc5215169db94776b47949ff20e6a1bdc0
nasirullahnu <nasirullah878@gmail.com>
2022-08-23 10:34:43 +0600

    all player select button added to the common function

--- js/players.js
+++ js/players.js
@@ -7,25 +7,44 @@
 
 // })
 
+function serials() {
+    var items = ['1.', '2.', '3.', "4.", "5."];
+    for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        console.log(item);
+        return item
+    }
+}
 
-function getPlayerName(playerName){
+
+
+function getPlayerName(playerName) {
     const yourComment = document.getElementById(playerName);
     const inputText = yourComment.innerText;
 
     const commentContainer = document.getElementById('sellected-players');
     const para = document.createElement('p');
-    para.innerText = inputText;
+    para.innerText = serials() + '.' + inputText;
     commentContainer.appendChild(para);
-
 }
 
 document.getElementById('btn-messi').addEventListener('click', function () {
     getPlayerName('player-1');
 })
-
-
-document.getElementById('btn-ronaldo').addEventListener('click',function(){
+document.getElementById('btn-ronaldo').addEventListener('click', function () {
     getPlayerName('player-2');
-    
 })
+document.getElementById('btn-neymar').addEventListener('click', function(){
+    getPlayerName('player-3');
+})
+document.getElementById('btn-maria').addEventListener('click', function(){
+    getPlayerName('player-4');
+})
+document.getElementById('btn-alves').addEventListener('click', function(){
+    getPlayerName('player-5');
+})
+document.getElementById('btn-kaka').addEventListener('click', function(){
+    getPlayerName('player-6');
+})
+
 
